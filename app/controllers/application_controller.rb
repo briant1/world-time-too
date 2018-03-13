4c980c3c4950b47219d9923010ab1964d502c993
Brian Thomas <brian.m.thomas@gmail.com>
2018-03-13 02:00:51 -0400

    redirect user to dashboard after sign in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
       redirect_to new_user_registration_path, :notice => 'You must sign up or sign in to view this page'
     end
   end
+  protected
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+  def after_sign_up_path_for(resource)
+    dashboard_path
+  end
 end
